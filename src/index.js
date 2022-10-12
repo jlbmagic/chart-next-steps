ed318092a34e3af1fde0d1b96c13013db3bbda8f
Jeremy Brown <jlbmagic@gmail.com>
2022-10-12 19:38:02 -0400

    got c3 chart working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-//Here we're importing items we'll need. You can add other imports here.
+import c3 from "c3";
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
+const options = {
+  bindto: "#chart",
+  data: {
+    colors: {
+      Apples: "#ff0000",
+      Peaches: "#00ff00",
+      Bananas: "#0000ff",
+    },
+    type: "bar",
+    columns: [
+      ["Apples", 90, 800, 600, 400, 850, 50],
+      ["Peaches", 50, 20, 10, 40, 15, 25],
+      ["Bananas", 350, 620, 410, 340, 215, 525],
+    ],
+  },
 };
-
-//
+const chart = c3.generate(options);
