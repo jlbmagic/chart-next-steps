4f6acbb7437509de9fe71cc7d2fe50a92b9e4090
Jeremy Brown <jlbmagic@gmail.com>
2022-10-12 20:56:32 -0400

    brought in config from FM

--- src/index.js
+++ src/index.js
@@ -1,45 +1,27 @@
 import c3 from "c3";
 
-const options = {
-  bindto: "#chart",
-  axis: {
-    x: { type: "category" },
-  },
-  data: {
-    colors: {
-      Apples: "#ff0000",
-      Peaches: "#00ff00",
-      Bananas: "#0000ff",
-    },
-    keys: { x: "name", value: ["Jan", "Feb", "Mar"] },
-    type: "area",
+window.loadChart = function (json) {
+  const obj = JSON.parse(json);
+  const data = obj.data;
+  const type = obj.type;
 
-    json: [
-      {
-        name: "Apples",
-        Jan: 327,
-        Feb: 437,
-        Mar: 111,
+  const options = {
+    bindto: "#chart",
+    size: { width: 600, height: 400 },
+    axis: {
+      x: { type: "category" },
+    },
+    data: {
+      colors: {
+        Apples: "#ff0000",
+        Peaches: "#00ff00",
+        Bananas: "#0000ff",
       },
-      {
-        name: "Peaches",
-        Jan: 555,
-        Feb: 454,
-        Mar: 222,
-      },
-      {
-        name: "Pears",
-        Jan: 242,
-        Feb: 864,
-        Mar: 343,
-      },
-      {
-        name: "Bananas",
-        Jan: 222,
-        Feb: 454,
-        Mar: 434,
-      },
-    ],
-  },
+      keys: { x: "name", value: ["Jan", "Feb", "Mar"] },
+      type: type,
+
+      json: data,
+    },
+  };
+  const chart = c3.generate(options);
 };
-const chart = c3.generate(options);
